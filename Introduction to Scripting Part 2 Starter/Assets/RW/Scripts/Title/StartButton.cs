9200b0052cb0fba272bfd79d6a259883e4a0f3b0
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2021-02-24 23:09:12 +0100

    title menu

---
+++ Introduction to Scripting Part 2 Starter/Assets/RW/Scripts/Title/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData) // 1
+    {
+        SceneManager.LoadScene("Game"); // 2
+    }
+
+}
