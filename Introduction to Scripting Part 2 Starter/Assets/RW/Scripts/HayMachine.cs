f3f7c4ee146a98b2beda41cff06adbfd9ecc3b9c
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2021-02-24 23:45:35 +0100

    static class haymachinecolor

--- Introduction to Scripting Part 2 Starter/Assets/RW/Scripts/HayMachine.cs
+++ Introduction to Scripting Part 2 Starter/Assets/RW/Scripts/HayMachine.cs
@@ -13,6 +13,38 @@ public class HayMachine : MonoBehaviour
     public float shootInterval; // 3
     private float shootTimer; // 4
 
+    public Transform modelParent; // 1
+
+    // 2
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
+
+    private void Start()
+    {
+        LoadModel();
+    }
+
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject); // 1
+
+        switch (GameSettings.hayMachineColor) // 2
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+                break;
+        }
+    }
+
     // Update is called once per frame
     private void Update()
     {
