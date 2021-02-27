177a093fefc4c7e13e90e724c9cf73da76bacaaa
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2021-02-27 16:17:41 +0100

    interactable objects

--- Kings Quest Clone Starter/Assets/RW/Scripts/GameManager.cs
+++ Kings Quest Clone Starter/Assets/RW/Scripts/GameManager.cs
@@ -34,20 +34,70 @@ using RayWenderlich.KQClone.Utilities;
 
 namespace RayWenderlich.KQClone.Core
 {
+    [System.Serializable]
+    public struct InteractableObjectLink
+    {
+        public string[] names;
+        public InteractableObject interactableObject;
+    }
+
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private InteractableObjectLink[] objectArray = null;
+        private UIManager uiManager;
+        private Dictionary<string, InteractableObject> sceneDictionary;
+
         public void ExecuteCommand(string command)
         {
             var parsedCommand = CommandParser.Parse(command);
-            Debug.Log($"Verb: {parsedCommand.verb}");
-            Debug.Log($"Primary: {parsedCommand.primaryEntity}");
-            Debug.Log($"Secondary: {parsedCommand.secondaryEntity}");
+
+            //1
+            if (string.IsNullOrEmpty(parsedCommand.verb))
+            {
+                uiManager.ShowPopup("Enter a valid command.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(parsedCommand.primaryEntity))
+            {
+                uiManager.ShowPopup("You need to be more specific.");
+                return;
+            }
+
+            if (sceneDictionary.ContainsKey(parsedCommand.primaryEntity))
+            {
+                //3
+                var sceneObject = sceneDictionary[parsedCommand.primaryEntity];
+                if (sceneObject.IsAvailable)
+                {
+                    if (parsedCommand.verb == "look") uiManager.ShowPopup(sceneObject.LookDialogue);
+                    else uiManager.ShowPopup(sceneObject.ExecuteAction(parsedCommand.verb));
+                }
+                else
+                {
+                    uiManager.ShowPopup("You can't do that - atleast not now.");
+                }
+            }
+            else
+            {
+                //2
+                uiManager.ShowPopup($"I don't understand '{parsedCommand.primaryEntity}'.");
+            }
         }
 
         private void Awake()
         {
-            ExecuteCommand("push the boulder using the wand");
+            uiManager = GameManager.FindObjectOfType<UIManager>();
+            sceneDictionary = new Dictionary<string, InteractableObject>();
+            foreach (var item in objectArray)
+            {
+                foreach (var name in item.names)
+                {
+                    sceneDictionary.Add(name.ToLowerInvariant().Trim(), item.interactableObject);
+                }
+            }
         }
+
 
     }
 }
