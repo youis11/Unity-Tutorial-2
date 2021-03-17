fd0c0677448e2b459baabb693d50062b533167a1
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2021-03-17 17:32:41 +0100

    Animation Tutorial

---
+++ Unit4_StarterFiles_2019.4/Assets/IKController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class IkController : MonoBehaviour
+{
+    public Transform objToPickUp;
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnAnimatorIK(int layerIndex)
+    {
+        float reach = animator.GetFloat("RightHandReach");
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, reach);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, objToPickUp.position);
+    }
+}
---
+++ Unit4_StarterFiles_2019.4/Assets/IKControllera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKControllera : MonoBehaviour
+{
+    public Transform objToPickUp;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnAnimatorIK(int layerIndex)    {
+        float reach = animator.GetFloat("RightHandReach");
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, reach);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, objToPickUp.position);
+    }
+}
